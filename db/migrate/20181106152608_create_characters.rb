bae498d30d57ea8fff62baa5b9819e68235a8856
Audrey <awill385@uncc.edu>
2018-11-06 15:26:44 +0000

    Generat Models

---
+++ db/migrate/20181106152608_create_characters.rb
@@ -0,0 +1,19 @@
+class CreateCharacters < ActiveRecord::Migration[5.2]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.strig :race
+      t.string :class
+      t.text :backstory
+      t.integer :strenth
+      t.integer :intelligence
+      t.integer :luck
+      t.integer :enduranc
+      t.integer :charisma
+      t.integer :perception
+      t.integer :agility
+
+      t.timestamps
+    end
+  end
+end
