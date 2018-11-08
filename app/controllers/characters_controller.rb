ee0438ca2968defdab55b9d3d3ca25814b89cfb8
Reginald Smith <rsmit281@uncc.edu>
2018-11-08 16:08:21 +0000

    Added form to character page and flavor text to main page.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,4 +1,23 @@
 class CharactersController < ApplicationController
     def new
+        @character = Character.new
+    end
+    
+    def create
+        @character = Character.new(character_params)
+        if @character.save
+            redirect_to @character
+        else
+            render 'new'
+        end
+    end
+    
+    def show
+        @character = Character.find(params[:id])
     end
 end
+
+private
+    def character_params
+        params.require(:character).permit(:name, :race, :roleType, :backstory, :strength, :intelligence, :luck, :endurance, :charisma, :perception, :agility)
+    end
