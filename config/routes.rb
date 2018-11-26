032507b33a6254e5fbd00e4aee5ae9fae34ef653
Audrey <awill385@uncc.edu>
2018-11-26 01:15:18 +0000

    add gatherers routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  
+  resources :characters do
+    resources :gatherers
+  end
+  
   root 'welcome#index'
-
-  resources :characters
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
