7f32801a88eb9b98ec5f8ecedd1f075fa320821e
Audrey <awill385@uncc.edu>
2018-12-01 23:48:00 +0000

    change user stories

--- spec/integration/blogger_adds_an_article_spec.rb
+++ spec/integration/blogger_adds_an_article_spec.rb
@@ -1,49 +1,43 @@
 require 'rails_helper.rb'
 
 feature "player adds a character" do
-    scenario "player navigates from the index page to the new character page" do
-      visit characters_path
-      click_link "New character"
-      expect(page).to have_content("RPG Character Maker")
-      expect(page).to have_field("Name")
-      expect(page).to have_field("Race")
-      expect(page).to have_field("Roletype")
-      expect(page).to have_field("Backstory")
-      expect(page).to have_field("Strength")
-      expect(page).to have_field("Perception")
-      expect(page).to have_field("Endurance")
-      expect(page).to have_field("Charisma")
-      expect(page).to have_field("Intelligence")
-      expect(page).to have_field("Agility")
-      expect(page).to have_field("Luck")
-  end
   
-  scenario "player adds a new character" do
+  scenario "As a user i want to be able to submit my character to the website" do
       visit new_character_path
       expect(page).to have_content("RPG Character Maker")
       fill_in "Name", with: "CHARACTER RSPEC TEST"
-      fill_in "Race", with: "HUMAN"
-      fill_in "Roletype", with: "CLERIC"
-      fill_in "Backstory", with: "BORN IN VILLAGE."
-      fill_in "Strength", with: "4"
-      fill_in "Perception", with: "5"
-      fill_in "Endurance", with: "6"
-      fill_in "Charisma", with: "7"
-      fill_in "Intelligence", with: "8"
-      fill_in "Agility", with: "9"
-      fill_in "Luck", with: "10"
+      fill_in "Race", with: "TEST"
+      fill_in "Roletype", with: "TEST"
+      fill_in "Backstory", with: "TEST."
       click_button "Create Character"
-      expect(page).to have_content("CHARACTER RSPEC TEST")
-      expect(page).to have_content("HUMAN")
-      expect(page).to have_content("CLERIC")
-      expect(page).to have_content("BORN IN VILLAGE.")
-      expect(page).to have_content("4")
-      expect(page).to have_content("5")
-      expect(page).to have_content("6")
-      expect(page).to have_content("7")
-      expect(page).to have_content("8")
-      expect(page).to have_content("9")
+  end
+  
+  scenario "as a user i want to be create a character with relevant stats" do
+      visit new_character_path
+      expect(page).to have_content("RPG Character Maker")
+      fill_in "Name", with: "JIMO"
+      fill_in "Race", with: "LION"
+      fill_in "Roletype", with: "WARRIOR"
+      fill_in "Backstory", with: "BORN IN TAKO YAKI."
+      fill_in "Strength", with: "10"
+      fill_in "Perception", with: "10"
+      fill_in "Endurance", with: "5"
+      fill_in "Charisma", with: "1"
+      fill_in "Intelligence", with: "3"
+      fill_in "Agility", with: "8"
+      fill_in "Luck", with: "1"
+      click_button "Create Character"
+      expect(page).to have_content("JIMO")
+      expect(page).to have_content("LION")
+      expect(page).to have_content("WARRIOR")
+      expect(page).to have_content("BORN IN TAKO YAKI.")
       expect(page).to have_content("10")
+      expect(page).to have_content("10")
+      expect(page).to have_content("5")
+      expect(page).to have_content("1")
+      expect(page).to have_content("3")
+      expect(page).to have_content("8")
+      expect(page).to have_content("1")
   end
   
 end
