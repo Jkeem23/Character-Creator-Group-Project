4c0a5d5f9e64e8d204654b1d31764f52ae9bc1cc
Lai <ssaepha1@uncc.edu>
2018-12-04 03:17:55 +0000

    Changed race and role to a select drop down list. Also created validation for character creation

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,18 @@
 class Character < ApplicationRecord
     has_many :gatherers, dependent: :destroy
+    
+    validates_length_of :name, 
+        :within => 3..12,
+        :too_short => 'Your name has to be longer than 3 characters',
+        :too_long => 'Your name has to be shorter than 12 characters'
+        :too_low
+    
+    validates :strength, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5, :message => "must be from 0 to 5"}
+    validates :perception, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5, :message => "must be from 0 to 5"}
+    validates :endurance, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5, :message => "must be from 0 to 5"}
+    validates :charisma, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5, :message => "must be from 0 to 5"}
+    validates :intelligence, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5, :message => "must be from 0 to 5"}
+    validates :agility, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5, :message => "must be from 0 to 5"}
+    validates :luck, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5, :message => "must be from 0 to 5"}
+    
 end
